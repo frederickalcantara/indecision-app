df893391d2969fb9aa1241c3c3d692d61ec928df
frederickalcantara <frederickaalcantara@gmail.com>
2018-01-12 20:01:55 -0500

    Fixing indecision app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.querySelector('#app'));
-
-// Improvements
-
-// Deploy to the web
-// Save the Data
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
