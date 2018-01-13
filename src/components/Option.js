8b3dc0344f218a59beff7b1d392b2d397d9b6d75
frederickalcantara <frederickaalcantara@gmail.com>
2018-01-12 20:08:12 -0500

    Capitalized remove

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,12 +4,12 @@ const Option = (props) => (
 	<div className="option">
 		<p className="option__text">{props.count}. {props.optionText}</p>
 		<button
-			className="button button--link" 
+			className="button button--link"
 			onClick={(e) => {
 				props.handleDeleteOption(props.optionText);
 			}}
 		>
-			remove
+			Remove
 		</button>
 	</div>
 );
