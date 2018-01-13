8e91cf879b9c3279f4aa046d301bf65902de6d3a
frederickalcantara <frederickaalcantara@gmail.com>
2018-01-12 19:34:55 -0500

    Setting up for deployment

--- src/app.js
+++ src/app.js
@@ -5,3 +5,8 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<p>This is my boilerplate</p>, document.querySelector('#app'));
+
+// Improvements
+
+// Deploy to the web
+// Save the Data
